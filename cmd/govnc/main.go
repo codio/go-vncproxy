593d238b5074c744964566297dc94f93a99dcaf7
Sergei Bronnikov <sergeibronnikov87@gmail.com>
2025-08-29 16:01:09 +0100

    build workflow

--- cmd/govnc/main.go
+++ cmd/govnc/main.go
@@ -12,7 +12,7 @@ import (
 
 const NoVncVersion = "v1.1.1"
 
-var indexHTML string
+var Version = "dev"
 
 func main() {
 	parser := argparse.NewParser("govnc", "VNCProxy for novnc")
@@ -20,11 +20,15 @@ func main() {
 	port := parser.Int("p", "port", &argparse.Options{Required: false, Default: 8080, Help: "VNC port"})
 	version := parser.String("v", "version", &argparse.Options{Required: false, Default: NoVncVersion, Help: "NOVNC client version"})
 	strictAuth := parser.Flag("a", "auth-strict", &argparse.Options{Required: false, Default: false, Help: "Enable strict authentication"})
+	showHelp := parser.Flag("h", "help", &argparse.Options{Required: false, Help: "Show help"})
 	err := parser.Parse(os.Args)
 
 	if err != nil {
-		fmt.Print(parser.Usage(err))
-		panic(err)
+		showHelpAndExit(parser, err, 1)
+	}
+
+	if *showHelp {
+		showHelpAndExit(parser, nil, 0)
 	}
 
 	runOpts := proxyserver.RunOpts{
@@ -38,6 +42,13 @@ func main() {
 		fmt.Println("Error running VNC proxy:", err)
 		os.Exit(1)
 	}
+}
+
+func showHelpAndExit(parser *argparse.Parser, err error, exitCode int) {
+	fmt.Println("VNCProxy for noVNC")
+	fmt.Println("Version: ", Version)
+	fmt.Print(parser.Usage(err))
+	os.Exit(exitCode)
 }
 
 func NewVNCProxy(vncport int) *vncproxy.Proxy {
---
+++ version-changed.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+BRANCH_NAME=${GITHUB_REF##*/}
+
+if [ "${BRANCH_NAME}" = "master" ]; then
+  echo "no need check master"
+else
+  echo "check if version was changed"
+  git cat-file -e origin/master:version
+  ret=$?
+  if [[ ${ret} -eq 0 ]]; then
+    git diff origin/master version | grep +baseVersion
+  else
+    exit ${ret}
+  fi
+fi
